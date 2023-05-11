f6b80ff9eea74fbd7677916ba8d6fee1250c4c5e
Emilian Murawski <murawski.emilian@gmail.com>
2023-05-12 00:32:50 +0200

    first stage

--- main.cpp
+++ main.cpp
@@ -1,11 +1,27 @@
 #include <iostream>
 #include "memory"
 #include <SDL_mixer.h>
+
 #define SDL_MAIN_HANDLED
 
 #include <SDL.h>
 
 using namespace std;
+
+int ballVelocitX = 5;
+int ballVelocitY = 5;
+int ballX = 10;
+int ballY = 10;
+int bkw = 800;
+int bkh = 600;
+int bkwmin = 0;
+int bkhmin = 0;
+int ball_width = 20;
+int ball_height = 20;
+
+int platf_weight = 100;
+int platf_height = 20;
+int platf_speed = 10;
 
 bool handle_events(SDL_Rect &rect) {
     SDL_Event e;
@@ -17,28 +33,41 @@ bool handle_events(SDL_Rect &rect) {
                 break;
             case SDL_KEYDOWN:
                 if (e.key.keysym.sym == SDLK_q) {
-                    std::cout<<"lag..." << std::endl ;
+                    std::cout << "lag..." << std::endl;
                     SDL_Delay(500);
                 }
-                if (e.key.keysym.sym == SDLK_ESCAPE){
+                if (e.key.keysym.sym == SDLK_ESCAPE) {
                     return false;
                 }
                 break;
         }
     }
-    if (key_state[SDL_SCANCODE_UP]) rect.y--;
-    if (key_state[SDL_SCANCODE_DOWN]) rect.y++;
-    if (key_state[SDL_SCANCODE_LEFT]) rect.x--;
-    if (key_state[SDL_SCANCODE_RIGHT]) rect.x++;
+//    if (key_state[SDL_SCANCODE_UP]) rect.y--;
+//    if (key_state[SDL_SCANCODE_DOWN]) rect.y++;
+    if (key_state[SDL_SCANCODE_LEFT] && rect.x> bkwmin)  rect.x+= -platf_speed;
+    if (key_state[SDL_SCANCODE_RIGHT] && rect.x<bkw - platf_weight) rect.x+= platf_speed;
 
     return true;
+}
+
+void moveBall(SDL_Rect &ball) {
+    ball.x += ballVelocitX;
+    ball.y += ballVelocitY;
+}
+
+void ball_collision(SDL_Rect &ball) {
+    if (ball.x < bkwmin || ball.x > bkw - ball_width) {
+        ballVelocitX = -ballVelocitX;
+    }
+    if (ball.y < bkhmin || ball.y > bkh - ball_height)
+        ballVelocitY = -ballVelocitY;
 }
 
 std::pair<std::shared_ptr<SDL_Window>, std::shared_ptr<SDL_Renderer>> create_context() {
     SDL_Window *window;
     SDL_Renderer *renderer;
     SDL_CreateWindowAndRenderer(
-            640, 480,
+            800, 600,
             SDL_WINDOW_SHOWN, &window, &renderer);
 
     std::shared_ptr<SDL_Window> w(window, [](SDL_Window *window) {
@@ -52,7 +81,7 @@ std::pair<std::shared_ptr<SDL_Window>, std::shared_ptr<SDL_Renderer>> create_con
     return {w, r};
 };
 
-std::shared_ptr<SDL_Texture> load_texture(shared_ptr<SDL_Renderer> renderer,const std::string name) {
+std::shared_ptr<SDL_Texture> load_texture(shared_ptr<SDL_Renderer> renderer, const std::string name) {
     auto surface = SDL_LoadBMP(("assets/" + name).c_str());
     if (!surface) {
         SDL_LogError(SDL_LOG_CATEGORY_APPLICATION, "Couldn't create surface from image: %s", SDL_GetError());
@@ -73,32 +102,29 @@ int main() {
     SDL_Init(SDL_INIT_EVERYTHING);
     {
         auto [window_p, renderer_p] = create_context();
-        SDL_Rect rect = {10, 10, 300, 100};
-        auto clouds = load_texture(renderer_p,"clouds.bmp");
-        auto background = load_texture(renderer_p,"background.bmp");
+        SDL_Rect ball = {ballX, ballY, ball_width, ball_height};
+        SDL_Rect platform = {350, 550, platf_weight, platf_height};
+        auto background = load_texture(renderer_p, "bg_space_seamless.bmp");
         auto prev_tick = SDL_GetTicks();
         int frame_dropped = 0;
-        while (handle_events(rect)) {
+        while (handle_events(platform)) {
             if (!frame_dropped) {
 //                SDL_SetRenderDrawColor(renderer_p.get(), 0, 0, 0, 255);
 //                SDL_RenderClear(renderer_p.get());
-                SDL_RenderCopy(renderer_p.get(),background.get(), nullptr, nullptr);
-                {
-                    int w;
-                    int h;
-                    SDL_QueryTexture(clouds.get(), NULL, NULL, &w, &h);
-                    SDL_Rect clouds_rect = {rect.x/2 - 200,  rect.y/2 - 200, w, h };
-                    SDL_RenderCopy(renderer_p.get(),clouds.get(), nullptr,  &clouds_rect);
-                }
+                SDL_RenderCopy(renderer_p.get(), background.get(), nullptr, nullptr);
+                moveBall(ball);
+                ball_collision(ball);
                 SDL_SetRenderDrawColor(renderer_p.get(), 255, 100, 50, 255);
-                SDL_RenderFillRect(renderer_p.get(), &rect);
+                SDL_RenderFillRect(renderer_p.get(), &ball);
+                SDL_RenderFillRect(renderer_p.get(), &platform);
                 SDL_RenderPresent(renderer_p.get());
+                SDL_RenderClear(renderer_p.get());
             }
-            auto ticks = SDL_GetTicks();
+            int ticks = SDL_GetTicks();
             if ((ticks - prev_tick) < 33) {
                 SDL_Delay(33 - (ticks - prev_tick));
-                frame_dropped= 0;}
-            else { frame_dropped = 1; }
+                frame_dropped = 0;
+            } else { frame_dropped = 1; }
             prev_tick += 33;
         }
     }
