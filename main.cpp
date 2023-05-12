6f98c7f8ce4aba9cd517bf0c6fc2a76c28c80263
Emilian Murawski <murawski.emilian@gmail.com>
2023-05-12 21:43:27 +0200

    second stage

--- main.cpp
+++ main.cpp
@@ -8,22 +8,34 @@
 
 using namespace std;
 
-int ballVelocitX = 5;
-int ballVelocitY = 5;
-int ballX = 10;
-int ballY = 10;
+int ballx = 10;
+int bally = 10;
+int ballvelx = 5;
+int ballvely = 5;
+
 int bkw = 800;
-int bkh = 600;
+int bkh= 600;
 int bkwmin = 0;
 int bkhmin = 0;
-int ball_width = 20;
-int ball_height = 20;
 
-int platf_weight = 100;
-int platf_height = 20;
-int platf_speed = 10;
+int batx = bkw/2;
+int baty = bkh - 30;
+int batSpeed = 10;
 
-bool handle_events(SDL_Rect &rect) {
+int brickw = 80;
+int brickh = 35;
+SDL_Rect brickrect [3][7];
+void InitializeBrick(){
+    for (int i = 0; i < 3; ++i) {
+        for (int j = 0; j < 7 ; ++j) {
+            int x = 80 + j * 90;
+            int y = 50 + i * 100;
+            brickrect[i][j] = {x, y, brickw, brickh};
+        }
+    }
+}
+
+bool handle_events() {
     SDL_Event e;
     auto *key_state = SDL_GetKeyboardState(nullptr);
     while (SDL_PollEvent(&e) != 0) {
@@ -44,23 +56,29 @@ bool handle_events(SDL_Rect &rect) {
     }
 //    if (key_state[SDL_SCANCODE_UP]) rect.y--;
 //    if (key_state[SDL_SCANCODE_DOWN]) rect.y++;
-    if (key_state[SDL_SCANCODE_LEFT] && rect.x> bkwmin)  rect.x+= -platf_speed;
-    if (key_state[SDL_SCANCODE_RIGHT] && rect.x<bkw - platf_weight) rect.x+= platf_speed;
+    if (key_state[SDL_SCANCODE_LEFT] && batx > 0 ) batx -= batSpeed;
+    if (key_state[SDL_SCANCODE_RIGHT] && batx < bkw - 60 ) batx += batSpeed;
 
     return true;
 }
 
-void moveBall(SDL_Rect &ball) {
-    ball.x += ballVelocitX;
-    ball.y += ballVelocitY;
+void moveBall()
+{
+    ballx += ballvelx;
+    bally += ballvely;
 }
 
-void ball_collision(SDL_Rect &ball) {
-    if (ball.x < bkwmin || ball.x > bkw - ball_width) {
-        ballVelocitX = -ballVelocitX;
+void ball_wall_colision(){
+    if (ballx < bkwmin || ballx > bkw - 30){
+        ballvelx = -ballvelx;
     }
-    if (ball.y < bkhmin || ball.y > bkh - ball_height)
-        ballVelocitY = -ballVelocitY;
+    if (bally < bkhmin || bally > bkh - 30){
+        ballvely = -ballvely;
+    }
+    int ballscaling = 25;
+    if (bally + ballscaling >= baty && bally + ballscaling <= baty+30 && ballx + ballscaling >=batx && ballx + ballscaling <= batx + 70){
+        ballvely = -ballvely;
+    }
 }
 
 std::pair<std::shared_ptr<SDL_Window>, std::shared_ptr<SDL_Renderer>> create_context() {
@@ -98,37 +116,52 @@ std::shared_ptr<SDL_Texture> load_texture(shared_ptr<SDL_Renderer> renderer, con
     return {texture, [](auto *p) { SDL_DestroyTexture(p); }};
 }
 
+void renderBricks(SDL_Renderer* renderer, SDL_Rect brick[][7]) {
+    for (int i = 0; i < 3; i++) {
+        for (int j = 0; j < 7; j++) {
+            SDL_RenderFillRect(renderer, &brick[i][j]);
+        }
+    }
+}
 int main() {
     SDL_Init(SDL_INIT_EVERYTHING);
     {
         auto [window_p, renderer_p] = create_context();
-        SDL_Rect ball = {ballX, ballY, ball_width, ball_height};
-        SDL_Rect platform = {350, 550, platf_weight, platf_height};
+        SDL_SetRenderDrawColor(renderer_p.get(), 255, 100, 50, 255);
+
         auto background = load_texture(renderer_p, "bg_space_seamless.bmp");
         auto prev_tick = SDL_GetTicks();
         int frame_dropped = 0;
-        while (handle_events(platform)) {
+        while (handle_events()) {
             if (!frame_dropped) {
-//                SDL_SetRenderDrawColor(renderer_p.get(), 0, 0, 0, 255);
-//                SDL_RenderClear(renderer_p.get());
+                SDL_Rect ballrect = {ballx,bally,20,30};
+
+                moveBall();
+                SDL_Rect batrect = {batx, baty, 60, 30};
+                ball_wall_colision();
+                InitializeBrick();
+
+
                 SDL_RenderCopy(renderer_p.get(), background.get(), nullptr, nullptr);
-                moveBall(ball);
-                ball_collision(ball);
-                SDL_SetRenderDrawColor(renderer_p.get(), 255, 100, 50, 255);
-                SDL_RenderFillRect(renderer_p.get(), &ball);
-                SDL_RenderFillRect(renderer_p.get(), &platform);
+                SDL_RenderFillRect(renderer_p.get(),&ballrect );
+                SDL_RenderFillRect(renderer_p.get(),&batrect );
+                for (int i = 0; i < 3; i++) {
+                    for (int j = 0; j < 7; j++) {
+                        SDL_RenderFillRect(renderer_p.get(),&brickrect[i][j]);
+                    }
+                }
                 SDL_RenderPresent(renderer_p.get());
                 SDL_RenderClear(renderer_p.get());
+                int ticks = SDL_GetTicks();
+                if ((ticks - prev_tick) < 33) {
+                    SDL_Delay(33 - (ticks - prev_tick));
+                    frame_dropped = 0;
+                } else { frame_dropped = 1; }
+                prev_tick += 33;
             }
-            int ticks = SDL_GetTicks();
-            if ((ticks - prev_tick) < 33) {
-                SDL_Delay(33 - (ticks - prev_tick));
-                frame_dropped = 0;
-            } else { frame_dropped = 1; }
-            prev_tick += 33;
         }
-    }
-    SDL_Quit();
+        SDL_Quit();
 
-    return 0;
+        return 0;
+    }
 }
