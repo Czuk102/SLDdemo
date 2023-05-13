18e4cbaa6bf3c51859f535d0171e873ef3438b67
Emilian Murawski <murawski.emilian@gmail.com>
2023-05-14 01:04:15 +0200

    third stage

--- main.cpp
+++ main.cpp
@@ -8,10 +8,10 @@
 
 using namespace std;
 
-int ballx = 10;
-int bally = 10;
+int ballx = 420;
+int bally = 500;
 int ballvelx = 5;
-int ballvely = 5;
+int ballvely = -5;
 
 int bkw = 800;
 int bkh= 600;
@@ -24,6 +24,8 @@ int batSpeed = 10;
 
 int brickw = 80;
 int brickh = 35;
+int deleted_bricks = 20;
+int number_of_bricks = 21;
 SDL_Rect brickrect [3][7];
 void InitializeBrick(){
     for (int i = 0; i < 3; ++i) {
@@ -66,6 +68,27 @@ void moveBall()
 {
     ballx += ballvelx;
     bally += ballvely;
+}
+
+bool ball_brick_collision_detect(SDL_Rect  rect1, SDL_Rect rect2){
+    if (SDL_HasIntersection(&rect1,&rect2)){
+        return true;
+    }else
+        return false;
+}
+void ball_brick_collision(SDL_Rect ball){
+    bool a;
+    for (int i = 0; i < 3; i++) {
+        for (int j = 0; j < 7; j++) {
+            a = ball_brick_collision_detect(brickrect[i][j], ball);
+            if (a){
+                brickrect[i][j].x=3000;
+                ballvely= -ballvely;
+                deleted_bricks++;
+            }a = false;
+        }
+
+    }
 }
 
 void ball_wall_colision(){
@@ -123,24 +146,37 @@ void renderBricks(SDL_Renderer* renderer, SDL_Rect brick[][7]) {
         }
     }
 }
+void win(SDL_Renderer* renderer){
+  SDL_Surface *win = SDL_LoadBMP("./assets/cup.bmp");
+    SDL_SetRenderDrawColor(renderer, 0, 0, 0, 0);
+  SDL_Texture *wintexture = SDL_CreateTextureFromSurface(renderer,win);
+  SDL_Rect winrect = {300,200,200,200};
+    SDL_RenderCopy(renderer,wintexture, nullptr,&winrect);
+    SDL_RenderPresent(renderer);
+    SDL_Delay(10000);
+}
+
 int main() {
     SDL_Init(SDL_INIT_EVERYTHING);
     {
         auto [window_p, renderer_p] = create_context();
         SDL_SetRenderDrawColor(renderer_p.get(), 255, 100, 50, 255);
-
+        InitializeBrick();
+//        auto winner = load_texture(renderer_p,"clouds.bmp");
         auto background = load_texture(renderer_p, "bg_space_seamless.bmp");
         auto prev_tick = SDL_GetTicks();
         int frame_dropped = 0;
         while (handle_events()) {
             if (!frame_dropped) {
-                SDL_Rect ballrect = {ballx,bally,20,30};
 
+                SDL_Rect ballrect = {ballx,bally,20,30};
                 moveBall();
                 SDL_Rect batrect = {batx, baty, 60, 30};
                 ball_wall_colision();
-                InitializeBrick();
-
+                ball_brick_collision(ballrect);
+                if (deleted_bricks>= number_of_bricks){
+                    win(renderer_p.get());
+                }
 
                 SDL_RenderCopy(renderer_p.get(), background.get(), nullptr, nullptr);
                 SDL_RenderFillRect(renderer_p.get(),&ballrect );
